5be57215b3f9f19c6f92526fd8d8abeb3c25a126
fabien <guigner.f@gmail.com>
2024-11-06 09:15:31 +0100

    [WIP] Machine state enemy

---
+++ Assets/Scripts/Enemies/CasualEnemy/States/StateAttack.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Unity.VisualScripting;
+
+using UnityEditorInternal;
+
+using UnityEngine;
+
+namespace Assets.Scripts.Enemies.CasualEnemy
+{
+    public class StateAttack : State
+    {
+        public StateAttack(StateMachineEnemy enemy) : base(enemy) { }
+
+        public override void OnEnter()
+        {
+            MachineEnemy.Rb2dEnemy.linearVelocity = Vector2.zero;
+            Debug.Log("JATAAAAAAAAAAK");
+        }
+
+        public override void OnUpdate()
+        {
+            //if (MachineEnemy.IsDead)
+            //{
+            //    MachineEnemy.ChangeState(StateMachineEnemy.STATE_DEAD);
+            //}
+            //else
+            //{
+            //    if (!MachineEnemy.IsMoving && !MachineEnemy.IsAttacking)
+            //    {
+            //        MachineEnemy.ChangeState(StateMachineEnemy.STATE_IDLE);
+            //    }
+            //    else if (MachineEnemy.IsMoving)
+            //    {
+            //        MachineEnemy.ChangeState(StateMachineEnemy.STATE_WALK);
+            //    }
+            //    else if (MachineEnemy.IsHit)
+            //    {
+            //        MachineEnemy.ChangeState(StateMachineEnemy.STATE_HIT);
+            //    }
+            //}
+        }
+
+        public override void OnExit()
+        {
+
+        }
+
+        public override void OnFixedUpdate()
+        {
+
+        }
+
+        public override void OnTriggerEnter()
+        {
+
+        }
+    }
+}
---
+++ Assets/Scripts/Enemies/CasualEnemy/States/StateHit.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Scripts.Enemies.CasualEnemy
+{
+    public class StateHit : State
+    {
+        public StateHit(StateMachineEnemy enemy) : base(enemy) { }
+
+        public override void OnEnter()
+        {
+
+        }
+
+        public override void OnUpdate()
+        {
+            if (MachineEnemy.IsDead)
+            {
+                MachineEnemy.ChangeState(StateMachineEnemy.STATE_DEAD);
+            }
+            else
+            {
+                if (!MachineEnemy.IsMoving)
+                {
+                    MachineEnemy.ChangeState(StateMachineEnemy.STATE_IDLE);
+                }
+                if (MachineEnemy.IsMoving)
+                {
+                    MachineEnemy.ChangeState(StateMachineEnemy.STATE_WALK);
+                }
+                else if (MachineEnemy.IsAttacking)
+                {
+                    MachineEnemy.ChangeState(StateMachineEnemy.STATE_ATTACK);
+                }
+            }
+        }
+
+        public override void OnExit()
+        {
+
+        }
+
+        public override void OnFixedUpdate()
+        {
+
+        }
+
+        public override void OnTriggerEnter()
+        {
+
+        }
+    }
+}
